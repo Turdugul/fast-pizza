48af1c2cd7f149fa1b53fbe9409a20d4ef15f4ae
Turdugul <okonbaevaturdugul@gmail.com>
2024-07-12 19:03:56 +0100

    fast-pizza home page

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const router = createBrowserRouter([
     errorElement: <Error />,
     children: [
       {
-        path: "/",
+        path: "/fast-pizza/",
         element: <Home />,
       },
       {
