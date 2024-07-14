1fcb7bfe68993fab50add437977b4cd308d28957
Turdugul <okonbaevaturdugul@gmail.com>
2024-07-14 19:05:15 +0100

    styled with tailwind css

---
+++ prettier.config.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  singleQuote: true,
+  // plugins: [prettierPluginTailwindcss],
+  plugins: ['prettier-plugin-tailwindcss'],
+  
+};
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,12 +1,15 @@
-import { Link } from "react-router-dom";
-import SearchOrder from "./SearchOrder";
+import { Link } from 'react-router-dom';
+import SearchOrder from './SearchOrder';
+import UserName from '../features/user/UserName';
 
 function Header() {
   return (
-    <header>
-      <Link to="/">Fast React Pizza Co</Link>
+    <header className="font-pizza flex items-center justify-between border-b border-stone-200 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
+      <Link to="/fast-pizza/" className="tracking-widest">
+        Fast React Pizza Co
+      </Link>
       <SearchOrder />
-      <p>Gul</p>
+      <UserName />
     </header>
   );
 }
